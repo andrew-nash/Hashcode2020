5fffc89826aee7996ebf1e5dec2278f7c898327b
misl <a.nash@outlook.ie>
2020-02-28 10:47:24 +0000

    Added more docunentation

---
+++ README.md
@@ -0,0 +1,18 @@
+# Hashcode 2020 Qualification Round Submission
+##  24,641,287 Points
+### UCC Team Demons
+
+## Approach
+The question states, given a collection of libraries, we need to to determine an optimal ordered subset from this collection, in order to maximise the total score obtained. Each library has a registration period before it can begin contributing a score, and no more than one library can be registering at any one time. see the full question here.
+
+
+The main idea behin our approach is, given the list of currently available libraries ans the amount of time remaining, we calculate for every library an average score per unit time that this library would providde, should it be the one we choose next. The average is caculated over the time from the current moment in time (start of the registration process) to the end of the entire time window. The average is weighted, in order to penalise long registration times, by making the average $\frac{\text{score}}{\text{penalty}\(\text{registration time}}\)+\(\text{time remaining} - \text{registration time}\)$.
+
+The default penalty that we used was 2, which we varied for multiple submissions, taking the maximum score for each. Hence, the included script may not achieve the exact score quoted above initially, without some variation in the penalty value. (I believe that some of the values we used included 1.25, 1.5, 3, 2.5, etc)
+
+Included are the output files that obtained the exact score.
+
+Note that this script is particularly slow to run on test case D, up to potentially a few minutes. Will look into improving this.
+
+
+
--- solution.py
+++ solution.py
@@ -1,17 +1,27 @@
+# the script runs slowest of test case D, so we decided to run it on this
+# case last each time, to allow us to analyse and submit the previous test
+# cases while waiting for execution to finish
+
 filenames = ["a_example", "b_read_on", "c_incunabula", "e_so_many_books", "f_libraries_of_the_world",  "d_tough_choices"]
 
+
 for fn in filenames:
+    # log the current file to track porgress
     print(fn)
     file = open(fn + ".txt", 'r')
     lines = [list(map(int, line.strip().split())) for line in file.readlines()]
     file.close()
 
-    penalty = 3
+    # alter this line to vary results, and to replicate,
+    # or indeed imporve on, our exact results
+    penalty = 2
     num_books = lines[0][0]
     num_lib = lines[0][1]
     time = num_days = lines[0][2]
     scores = lines[1]
     score = 0
+    
+    # track what books and libraries have already been used
     duplicates = set([])
     libraries_used = set([])
     schedule = []
@@ -21,40 +31,61 @@ for fn in filenames:
     for i in range(2, len(lines), 2):
         if i==N-1 and lines[i]==[]:
             break
-        #print(lines[i], i, len(lines))
+        # for every library, we want to sort its books in decreasing order of score
+        # while also retaining the index corresponding to each score
         bidxs = sorted([(scores[lines[i+1][j]], lines[i+1][j]) for j in range(len(lines[i+1]))])[::-1]
         books_idxs = [x[1] for x in bidxs]
         libraries.append((lines[i][0], lines[i][1], lines[i][2], books_idxs, (i-2)//2))
 
     while time > 0:
-
+        # while there is time available to make a registration
         lib_scores = []
+        max_val = -1
 
         for lib in libraries:
             if lib[-1] in libraries_used:
+                # cant register a library twice
                 continue
-
+            # after registration, check we have time to scan some books
+            # and if so, how many
             books_scanned = (time-lib[1])*lib[2]
             books_remaining = books_scanned
             if books_scanned < 0:
                 continue
-            #print(lib)
+
             books = [(scores[lib[3][i]],lib[3][i]) for i in range(lib[0])]
             i = -1
             books_taken = []
             score = 0
+            # we will terminate if we either
+            # run out of books to scan
+            # or we run out of time
             while books_remaining >0 and i < (lib[0]-1):
                 i += 1
-
+                # we don't want to count a book towards the score if it has already been seen
                 if books[i][1] not in duplicates:
                     score+=books[i][0]
                     books_taken.append(books[i][1])
                     #books_taken.append()
 
                     books_remaining-=1
+            # calculate the weighted average
             value = score/((time-lib[1])+(penalty*lib[1]))
-            lib_scores.append([value,books_taken, lib[1], lib[-1]])
-        lib_scores.sort()
+            # if this score is better than the best score seen so far
+            # we will plan to tke this library
+            if value>max_val:
+                lib_scores = [[value,books_taken, lib[1], lib[-1]]]
+                max_val = value
+
+            # we initially tracked scores and information for all libraries
+            # and sorted afterwards to take the max
+
+            # This is clearly inefficient, and instead, we revised it to
+            # update a max at each iteration
+
+            #lib_scores.append([value,books_taken, lib[1], lib[-1]])
+        #lib_scores.sort()
+        # there are no possible libraries to choose
         if len(lib_scores) == 0:
             break
         books_taken = lib_scores[-1][1]
@@ -78,19 +109,3 @@ for fn in filenames:
 
         f.write(str(lib_idx)+' '+str(no_books)+'\n')
         f.write(books+'\n')
-
-
-
-
-
-
-
-
-
-
-    # for line in lines:
-    #     print(line)
-    # print("scores", scores)
-    # break
-
-
